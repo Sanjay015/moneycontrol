b820ea50b21039b23aa7e0bcd57bbf0c9a75a2ea
sanjay <sanjay.cc.in@gmail.com>
2018-07-30 15:48:04 +0530

    ENH: Added database schema for data

--- config.py
+++ config.py
@@ -2,68 +2,84 @@
 Configuration file.
 """
 
-
+# User can update db user name
 USER = "root"
+# User can update db hostname
 HOST = '127.0.0.1'
+# User can update db password
 PASSWORD = "admin"
+# User can update db port number
 DATABASE_PORT = '3306'
+# User can update db database name
 DATABASE_NAME = "moneycontrol"
 
+# URL to fetch list of companies
+QUOTE_URL = 'http://www.moneycontrol.com/india/stockpricequote'
+
+# Query to create schema if not exists
 SCHEMA_CREATE_QUERY = "create schema if not exists `{schema}` default character set utf8"
 
+# Query to create table schema in database
 TABLE_CREATE_QUERY = """
     create table if not exists `{schema}`.`company_data` (
-    `company_name` varchar(200) not null primary key,
+    `company_name` varchar(200) not null,
+    `company_title` varchar(200) not null primary key,
     `company_sector` varchar(200) null,
     `company_url` varchar(2000) null,
-    `market_cap_(rs_cr)` decimal null,
-    `p/e` decimal null,
-    `book_value_(rs)` decimal null,
-    `eps_(ttm)` decimal null,
-    `face_value_(rs)` decimal null,
-    `industry_p/e` decimal null,
-    `p/c` decimal null,
-    `price/book` decimal null,
+    `market_cap_rs_cr` float null,
+    `pe` float null,
+    `book_value_rs` float null,
+    `eps_ttm` float null,
+    `face_value_rs` float null,
+    `industry_pe` float null,
+    `pc` float null,
+    `pricebook` float null,
     `updated_on` datetime null
     ) engine = innodb default character set = utf8"""
 
+# Query to drop and re create trigger if already exists.
 DROP_TRIGGER_QUERY = """DROP TRIGGER IF EXISTS {schema}.company_data_before_insert;"""
 
+# Query to create trigger, to make sure companies outside predefined list
+# should not get inserted to database before insert
 TRIGGER_CREATE_QUERY = """
     CREATE DEFINER = CURRENT_USER TRIGGER
     `{schema}`.`company_data_before_insert` BEFORE INSERT ON `{schema}`.`company_data` FOR EACH ROW
     BEGIN
         declare msg varchar(128);
         if new.company_name not in {company_list} then
-            set msg = 'you are trying to insert a value which is out of defined list.';
+            set msg = 'You are trying to insert a value which is out of defined list.';
             signal sqlstate '45000' set message_text = msg;
         end if;
     END
     """
 
-IS_EMPTY_TABLE = """select exists (select company_name from `moneycontrol`.`company_data`) as data_exists"""
+# Query to fetch companies list from table
+GET_COMPANY_LIST = """select company_url from `{}`.`company_data`"""
 
+# Query to load companies in database
 INSERT_UPDATE_COMPANY_LIST = """
-    insert into `{schema}`.`company_data` (company_name, company_sector, company_url)
+    insert into `{schema}`.`company_data` (company_name, company_title, company_sector, company_url)
     values {company_insert} on duplicate key update
     company_sector=values(company_sector),
     company_url=values(company_url)
     """
 
-INSERT_UPDATE_COMPANY_DATA = """
-    insert into `{schema}`.`company_data`
-    (company_name, market_cap_(rs_cr), p/e, book_value_(rs), eps_(ttm), face_value_(rs), industry_p/e, p/c, price/book)
-    values ({data_insert})
-    on duplicate key update
-    market_cap_(rs_cr)=values(market_cap_(rs_cr)),
-    p/e=values(p/e),
-    book_value_(rs)=values(book_value_(rs)),
-    eps_(ttm)=values(eps_(ttm)),
-    face_value_(rs)=values(face_value_(rs)),
-    industry_p/e=values(industry_p/e),
-    p/c=values(p/c),
-    price/book=values(price/book),
-    updated_on=values(updated_on)
+# Query to update company records in database
+UPDATE_COMPANY_DATA = """
+    update `{schema}`.`company_data`
+    set
+    market_cap_rs_cr={market_cap_rs_cr},
+    pe={pe},
+    book_value_rs={book_value_rs},
+    eps_ttm={eps_ttm},
+    face_value_rs={face_value_rs},
+    industry_pe={industry_pe},
+    pc={pc},
+    pricebook={pricebook},
+    updated_on='{updated_on}'
+    where company_name = '{company_name}'
     """
 
-COMPANY_LIST = ('3M India', '8K Miles Soft', 'Aarti Ind', 'Aban Offshore', 'ABB India', 'Abbott India', 'ACC', 'Adani Enterpris', 'Adani Ports', 'Adani Power', 'Adani Trans', 'Aditya Birla F', 'Advanced Enzyme', 'Aegis Logistics', 'AIA Engineering', 'Ajanta Pharma', 'Akzo Nobel', 'Alembic Pharma', 'Alkem Lab', 'Allahabad Bank', 'Allcargo', 'Amara Raja Batt', 'Ambuja Cements', 'Andhra Bank', 'Apar Ind', 'APL Apollo', 'Apollo Hospital', 'Apollo Tyres', 'Arvind', 'Asahi India', 'Ashok Leyland', 'Ashoka Buildcon', 'Asian Paints', 'Astra Microwave', 'Astral Poly Tec', 'AstraZeneca', 'Atul', 'Aurobindo Pharm', 'Avanti Feeds', 'Avenue Supermar', 'Axis Bank', 'Bajaj Auto', 'Bajaj Corp', 'Bajaj Electric', 'Bajaj Finance', 'Bajaj Finserv', 'Bajaj Hind', 'Bajaj Holdings', 'Balkrishna Ind', 'Balmer Lawrie', 'Balrampur Chini', 'Bank of Baroda', 'Bank of India', 'Bank of Mah', 'BASF', 'Bata India', 'Bayer CropScien', 'BEML', 'Berger Paints', 'BF Utilities', 'Bharat Elec', 'Bharat Fin', 'Bharat Forge', 'Bharti Airtel', 'Bharti Infratel', 'BHEL', 'Biocon', 'Birla Corp', 'Bliss GVS', 'Blue Dart', 'Blue Star', 'Bombay Burmah', 'Bombay Dyeing', 'Bosch', 'BPCL', 'Britannia', 'Cadila Health', 'Can Fin Homes', 'Canara Bank', 'Capital First', 'Caplin Labs', 'Carborundum', 'CARE', 'Castrol', 'CCL Products', 'Ceat', 'Central Bank', 'Century', 'CenturyPlyboard', 'Cera Sanitary', 'CESC', 'CG Consumer', 'CG Power', 'Chambal Fert', 'Chennai Petro', 'Cholamandalam', 'Cipla', 'City Union Bank', 'Coal India', 'Coffee Day', 'Colgate', 'Container Corp', 'Coromandel Int', 'Corporation Bk', 'Cox & Kings', 'CRISIL', 'Cummins', 'Cyient', 'Dabur India', 'Dalmia Bharat', 'DB Corp', 'DCB Bank', 'DCM Shriram', 'Deepak Fert', 'Delta Corp', 'Den Networks', 'Dena Bank', 'Dewan Housing', 'Dhanuka Agritec', 'Dilip Buildcon', 'Dish TV', 'Divis Labs', 'DLF', 'Dr Lal PathLab', 'Dr Reddys Labs', 'eClerx Services', 'Edelweiss', 'Eicher Motors', 'EID Parry', 'EIH', 'Elgi Equipments', 'Emami', 'Endurance Techn', 'EngineersInd', 'Ent Network Ind', 'Equitas Holding', 'Eros Intl', 'Escorts', 'Essel Propack', 'Eveready Ind', 'Exide Ind', 'FDC', 'Federal Bank', 'Finolex Cables', 'Finolex Ind', 'Firstsource Sol', 'Force Motors', 'Fortis Health', 'Future Consumer', 'Future Life', 'Future Retail', 'GAIL', 'Gateway Distri', 'Gati', 'Gayatri Project', 'GE Power India', 'GE Shipping', 'GE T&D; India', 'GIC Housing Fin', 'Gillette India', 'GlaxoSmith Con', 'GlaxoSmithKline', 'Glenmark', 'GMR Infra', 'GNFC', 'Godfrey Phillip', 'Godrej Consumer', 'Godrej Ind', 'Godrej Prop', 'Granules India', 'Grasim', 'Greaves Cotton', 'Greenply Ind', 'Grindwell Norto', 'GRUH Finance', 'GSFC', 'Guj Alkali', 'Guj Flourochem', 'Guj Heavy Chem', 'Guj Mineral', 'Guj State Petro', 'Gujarat Gas', 'Gujarat Pipavav', 'Gulf Oil Lubric', 'Hathway Cable', 'Hatsun Agro', 'Havells India', 'HCL Info', 'HCL Tech', 'HDFC', 'HDFC Bank', 'HDIL', 'Heidelberg Cem', 'Heritage Foods', 'Hero Motocorp', 'Hexaware Tech', 'HFCL', 'Himatsingka Sei', 'Hind Constr', 'Hind Copper', 'Hind Zinc', 'Hindalco', 'Honeywell Autom', 'HPCL', 'HSIL', 'HUL', 'ICICI Bank', 'ICICI Prudentia', 'ICRA', 'IDBI Bank', 'Idea Cellular', 'IDFC', 'IDFC Bank', 'IFCI', 'IGL', 'IIFL Holdings', 'ILandFS Trans', 'India Cements', 'Indiabulls Hsg', 'Indiabulls Real', 'Indiabulls Vent', 'Indian Bank', 'Indian Hotels', 'Indo Count', 'Indoco Remedies', 'IndusInd Bank', 'Infibeam Incorp', 'Info Edge', 'Infosys', 'Ingersoll Rand', 'INOX Leisure', 'Inox Wind', 'Intellect Desig', 'Interglobe Avi', 'IOB', 'IOC', 'Ipca Labs', 'IRB Infra', 'ISGEC Heavy Eng', 'ITC', 'ITD Cementation', 'J Kumar Infra', 'J. K. Cement', 'JagranPrakashan', 'Jai Corp', 'Jain Irrigation', 'Jaiprakash Asso', 'JB Chemicals', 'JBF Industries', 'Jet Airways', 'Jindal (Hisar)', 'Jindal PolyFilm', 'Jindal Saw', 'Jindal Steel', 'JK Bank', 'JK Lakshmi Cem', 'JK Tyre & Ind', 'JM Financial', 'Johnson Control', 'JSW Energy', 'JSW Steel', 'Jubilant Food', 'Jubilant Life', 'Just Dial', 'Jyothy Labs', 'Kajaria Ceramic', 'Kalpataru Power', 'Kansai Nerolac', 'Karnataka Bank', 'Kaveri Seed', 'KEC Intl', 'Kesoram', 'Kirloskar Oil', 'Kitex Garments', 'Kotak Mahindra', 'KPIT Tech', 'KPR Mill', 'KRBL', 'Kwality', 'L&T; Finance', 'L&T; Infotech', 'L&T; Technology', 'La Opala RG', 'Lakshmi Machine', 'Lakshmi Vilas', 'Larsen', 'Laurus Labs', 'LIC Housing Fin', 'Linde India', 'Lupin', 'M&M;', 'M&M; Financial', 'Magma Fincorp', 'Mahanagar Gas', 'Mahindra CIE', 'Mahindra Holida', 'Mahindra Life', 'Manappuram Fin', 'Manpasand Bever', 'Marico', 'Marksans Pharma', 'Maruti Suzuki', 'Max Financial', 'Max India', 'Mcleod', 'MCX India', 'Minda Ind', 'Mindtree', 'MMTC Ltd', 'MOIL', 'Monsanto India', 'Motherson Sumi', 'Motilal Oswal', 'MphasiS', 'MRF', 'MRPL', 'MTNL', 'Muthoot Finance', 'NALCO', 'Narayana Hruda', 'Natco Pharma', 'Nava Bharat Ven', 'Navin Fluorine', 'Navkar Corp', 'Navneet', 'NBCC (India)', 'NCC', 'Nestle', 'Network 18', 'NHPC', 'NIIT', 'NIIT Tech', 'Nilkamal', 'NLC India', 'NMDC', 'NTPC', 'Oberoi Realty', 'OCL India', 'Oil India', 'Omaxe', 'ONGC', 'Oracle Fin Serv', 'Orient Cement', 'Oriental Bank', 'P and G', 'Page Industries', 'Parag Milk Food', 'PC Jeweller', 'Persistent', 'Petronet LNG', 'Pfizer', 'Phoenix Mills', 'PI Industries', 'Pidilite Ind', 'Piramal Enter', 'PNB', 'PNB Housing Fin', 'PNC Infratech', 'Polaris Consult', 'Power Finance', 'Power Grid Corp', 'Praj Industries', 'Prestige Estate', 'Prism Cement', 'PTC India', 'PTC India Fin', 'PVR', 'Radico Khaitan', 'Rain Industries', 'Rajesh Exports', 'Rallis India', 'Ramco Cements', 'Rashtriya Chem', 'Ratnamani Metal', 'Rattan Power', 'Raymond', 'RBL Bank', 'REC', 'Redington', 'Rel Capital', 'Relaxo Footwear', 'Reliance', 'Reliance Comm', 'Reliance Infra', 'Reliance Naval', 'Reliance Power', 'Religare Enterp', 'Repco Home', 'Rolta', 'S H Kelkar', 'Sadbhav Engg', 'SAIL', 'Sanofi India', 'SBI', 'Schaeffler Ind', 'Schneider Infra', 'Sequent Scienti', 'Sharda Crop', 'Sheela Foam', 'Shilpa', 'Shipping Corp', 'Shoppers Stop', 'Shree Cements', 'Shree Renuka', 'Shriram City', 'Shriram Trans', 'Siemens', 'Siti Networks', 'SJVN', 'SKF India', 'SML Isuzu', 'Sobha', 'Solar Ind', 'Somany Ceramics', 'Sonata', 'South Ind Bk', 'SpiceJet', 'SREI Infra', 'SRF', 'Sterlite Techno', 'Strides Shasun', 'Sudarshan Chem', 'Sun Pharma', 'Sun Pharma Adv', 'Sun TV Network', 'Sundram', 'Sunteck Realty', 'Supreme Ind', 'Suven Life Sci', 'Suzlon Energy', 'Swan Energy', 'Symphony', 'Syndicate Bank', 'Syngene Intl', 'Take Solutions', 'Tamil Newsprint', 'Tata Chemicals', 'Tata Coffee', 'Tata Comm', 'Tata Elxsi', 'Tata Global Bev', 'Tata Inv Corp', 'Tata Motors', 'Tata Motors (D)', 'Tata Power', 'Tata Sponge', 'Tata Steel', 'TCS', 'Tech Mahindra', 'Techno Electric', 'Texmaco Rail', 'Thermax', 'Thomas Cook', 'Thyrocare Techn', 'TI Financial', 'Timken', 'Titagarh Wagons', 'Titan Company', 'Torrent Pharma', 'Torrent Power', 'Trent', 'Trident', 'Triveni Turbine', 'TTK Prestige', 'TV TodayNetwork', 'TV18 Broadcast', 'TVS Motor', 'TVS Srichakra', 'UCO Bank', 'Uflex', 'Ujjivan Financi', 'UltraTechCement', 'Unichem Labs', 'Union Bank', 'Unitech', 'United Brewerie', 'UPL', 'V-Guard Ind', 'Va Tech Wabag', 'Vakrangee', 'Vardhman Text', 'Varun Beverages', 'Vedanta', 'Videocon Ind', 'Vijaya Bank', 'Vinati Organics', 'VIP Industries', 'Voltas', 'VST', 'WABCO India', 'Welspun Corp', 'Welspun India', 'Westlife Dev', 'Whirlpool', 'Wipro', 'Wockhardt', 'Wonderla', 'Yes Bank', 'Zee Entertain', 'Zensar Tech')
+# List of companies for which we should be able to insert data
+COMPANY_LIST = ('3mindia', '8kmilessoftwareservices', 'aartiindustries', 'abanoffshore', 'abbindia', 'abbottindia', 'acc', 'adanienterprises', 'adaniportsspecialeconomiczone', 'adanipower', 'adanitransmission', 'adityabirlafashionretail', 'advancedenzymetechnologies', 'aegislogistics', 'aiaengineering', 'ajantapharma', 'akzonobelindia', 'alembicpharmaceuticals', 'alkemlaboratories', 'allahabadbank', 'allcargologistics', 'amararajabatteries', 'ambujacements', 'andhrabank', 'aparindustries', 'aplapollotubes', 'apollohospitalsenterprises', 'apollotyres', 'arvind', 'asahiindiaglass', 'ashokleyland', 'ashokabuildcon', 'asianpaints', 'astramicrowaveproducts', 'astralpolytechnik', 'astrazenecapharma', 'atul', 'aurobindopharma', 'avantifeeds', 'avenuesupermarts', 'axisbank', 'bajajauto', 'bajajcorp', 'bajajelectricals', 'bajajfinance', 'bajajfinserv', 'bajajhindusthan', 'bajajholdingsinvestment', 'balkrishnaindustries', 'balmerlawriecompany')
--- crawler.py
+++ crawler.py
@@ -1,29 +1,21 @@
 """
 Crawler module for money control.
 """
+import os
 import re
+import sys
 import config
+import logging
 import requests
+import argparse
+import sqlalchemy
 import tornado.web
 import pandas as pd
 import tornado.ioloop
+from datetime import datetime
 from bs4 import BeautifulSoup
 from tornado import httpclient
-from sqlalchemy import create_engine
 
-
-class AsyncContextManager(object):
-    """
-    Context manager to perform asyn http call.
-    Created context manager to close async http call even if there is exception.
-    """
-
-    def __enter__(self):
-        self.http_client = httpclient.AsyncHTTPClient()
-        return self.http_client
-
-    def __exit__(self, errortype, value, traceback):
-        self.http_client.close()
 
 
 class DBManagement(object):
@@ -33,6 +25,9 @@ class DBManagement(object):
     """
 
     def execute(self, query):
+        """
+        Execute database queries.
+        """
         return self.conn.execute(query).execution_options(autocommit=True)
 
     def __enter__(self):
@@ -43,10 +38,9 @@ class DBManagement(object):
                             'schema': config.DATABASE_NAME,
                             'company_list': config.COMPANY_LIST}
         self.conn_str = self.conn_str.format(**self.conn_config)
-        self.engine = create_engine(self.conn_str)
+        self.engine = sqlalchemy.create_engine(self.conn_str)
         self.conn = self.engine.connect()
         return self
-
 
     def __exit__(self, errortype, value, traceback):
         self.conn.close()
@@ -63,10 +57,15 @@ class CrawlHandler(tornado.web.RequestHandler):
         Like create schema/table/triggers.
         DB config and queries are stored in `config.py`.
         """
+        logger = logging.getLogger('MONEYCONTROLAPP')
         with DBManagement() as dbmanager:
             for query in [config.SCHEMA_CREATE_QUERY, config.TABLE_CREATE_QUERY,
                           config.DROP_TRIGGER_QUERY, config.TRIGGER_CREATE_QUERY]:
-                dbmanager.conn.execute(query.format(**dbmanager.conn_config))
+                query = query.format(**dbmanager.conn_config)
+                try:
+                    dbmanager.conn.execute(query)
+                except Exception as ex:
+                    logger.error('Failed to execute query {} due to {}'.format(query.replace('\n', ' '), ex.args))
 
     def parse_number(self, strnum):
         """
@@ -74,70 +73,149 @@ class CrawlHandler(tornado.web.RequestHandler):
         """
         pattern = "[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?"
         regex = re.findall(pattern, strnum)
-        return float(regex[0]) if regex else None
+        return float(regex[0].replace(',', '')) if regex else 'null'
 
-    def get_company_list(self):
+    def get_company_url(self, dbmanager):
         """
         Get the lists of companies and push them to database table.
         This is a one time job.
         """
-        url = 'http://www.moneycontrol.com/india/stockpricequote'
+        url = config.QUOTE_URL
+        logger = logging.getLogger('MONEYCONTROLAPP')
         response = requests.get(url)
-        with DBManagement() as dbmanager:
-            # Checking if company list exists or not.
-            df = pd.read_sql(config.IS_EMPTY_TABLE, dbmanager.conn_str)
-            if len(df) > 0 and df['data_exists'].tolist()[0] == 1:
-                print('Company list already exists. Skip to fetch company list.')
-                return
-            print('Company list not avalable. Started fetching company list.')
-            if response.status_code == 200:
-                soup = BeautifulSoup(response.content, 'html.parser')
-                table = soup.find('table', {'class':'pcq_tbl MT10'})
-                rows = table.find_all('td')
-                for row in rows:
-                    row = row.find('a')
-                    company_url, company_name = row['href'], row.get_text()
-                    if company_name:
-                        company_sector = company_url.replace(url, '').split('/')[1].strip()
-                        dbmanager.conn_config['company_insert'] = (company_name, company_sector, company_url)
-                        dbmanager.conn.execute(config.INSERT_UPDATE_COMPANY_LIST.format(**dbmanager.conn_config))
+        if response.status_code == 200:
+            # Parsing html content.
+            soup = BeautifulSoup(response.content, 'html.parser')
+            table = soup.find('table', {'class':'pcq_tbl MT10'})
+            rows = table.find_all('td')
+            row_count = 0
+            for row in rows:
+                row = row.find('a')
+                # Extracting company's url and company name.
+                company_url, company_title = row['href'], row.get_text()
+                if company_title:
+                    company_sector = company_url.split('/')[-3].strip()
+                    company_name = company_url.split('/')[-2].strip()
+                    # Constructing query
+                    dbmanager.conn_config['company_insert'] = (company_name, company_title, company_sector, company_url)
+                    # Pushing companies to database table
+                    query = config.INSERT_UPDATE_COMPANY_LIST.format(**dbmanager.conn_config)
+                    try:
+                        dbmanager.conn.execute(query)
+                        row_count += 1
+                    except sqlalchemy.exc.OperationalError as ex:
+                        logger.error('Failed to execute query {} due to {}'.format(query.replace('\n', ' '), ex.args))
+                    except Exception as ex:
+                        logger.error('Failed to execute query {} due to {}'.format(query.replace('\n', ' '), ex.args))
+            logger.debug('Inserted total {} rows(company list) to database.'.format(row_count))
+        else:
+            logger.debug('Unable to connect to {}, due to {} error code and {} reason.'.format(
+                url, response.status_code, response.reason))
 
-    def get_company_data(self, content):
+    def get_company_stats(self, content, company_name):
         """
         Get all required data for a company as per requirement.
         Push the database to DB.
         """
+        logger = logging.getLogger('MONEYCONTROLAPP')
+        # Parsing html content.
         soup = BeautifulSoup(content, 'html.parser')
         divs = soup.find('div', {'id': 'mktdet_1'})
         divs = divs.find_all('div', {'class': 'PA7 brdb'})
         row = {}
-        requiered_columns = ['book_value_(rs)', 'eps_(ttm)',
-                             'face_value_(rs)', 'industry_p/e',
-                             'market_cap_(rs_cr)', 'p/c', 'p/e',
-                             'price/book']
+        # List of required columns which should pushed to DB.
+        requiered_columns = ['book_value_rs', 'eps_ttm', 'face_value_rs', 'industry_pe',
+                             'market_cap_rs_cr', 'pc', 'pe', 'pricebook']
+        # Parsing statistics of company.
         for div in divs:
             name = div.find('div', {'class': 'FL gL_10 UC'})
             value = div.find('div', {'class': 'FR gD_12'})
             if name and value:
-                name = name.get_text().lower().replace(' ', '_')
+                name = name.get_text().lower().strip()
+                name = name.replace('(', '').replace(')', '')
+                name = name.replace(' ', '_').replace('%', '').replace('/', '')
+                # Add only required columns.
                 if name in requiered_columns:
                     row[name] = self.parse_number(value.get_text())
+        # Maintaining dict to format the query
+        row['updated_on'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        row['company_name'] = company_name
+        row['schema'] = config.DATABASE_NAME
+        # Connect to db and update the records
+        with DBManagement() as dbmanager:
+            # Updating stats for company in table.
+            try:
+                dbmanager.conn.execute(config.UPDATE_COMPANY_DATA.format(**row))
+            except Exception as ex:
+                logger.error('Failed to execute query {} due to {}'.format(query.replace('\n', ' '), ex.args))
 
-    def get_soup(self, response):
+    def get_data(self, response):
         """
         Callback method for async http call.
         """
-        if response.code == 200:
-            self.get_company_data(self, response.body)
+        logger = logging.getLogger('MONEYCONTROLAPP')
+        company_url = response.effective_url
+        company_name = company_url.split('/')[-2].strip()
+        # If any error in response logging it
+        if response.error:
+            logger.error('Content no longer available for company "{}" url: {}, error {}'.format(
+                company_name, company_url, response.error))
+        else:
+            logger.debug('Updating records for company "{}" from url: {}'.format(company_name, company_url))
+            # Parsing the company data.
+            self.get_company_stats(response.body, company_name)
 
     def get(self):
+        logger = logging.getLogger('MONEYCONTROLAPP')
+        logger.debug('Running initial DB setup if required.')
+        # Initial database setup.
         self.setup()
-        self.get_company_list()
-        # Yet to implement company
-        # with AsyncContextManager() as http_client:
-        #     for url in ['https://www.google.com', 'https://www.youtube.com']:
-        #         http_client.fetch(url, callback=self.get_soup)
-        # self.write("Fetching results for: {}\n".format(url))
+
+        with DBManagement() as dbmanager:
+            # Checking if company list exists or not.
+            query = config.GET_COMPANY_LIST.format(config.DATABASE_NAME)
+            company_list = pd.read_sql(query, dbmanager.conn_str)
+            # If list exists we will simply go and look for companies stats.
+            # Otherwise will fetch the list of companies.
+            if len(company_list) > 0:
+                logger.warn('Company list already exists. Skip to fetch company list.')
+            else:
+                logger.warn('Company list not avalable. Started fetching company list.')
+                # Get companies list.
+                self.get_company_url(dbmanager)
+                company_list = pd.read_sql(query, dbmanager.conn_str)
+
+        # Making async http call to fetch companies stats without http request block.
+        http_client = httpclient.AsyncHTTPClient()
+        for url in company_list['company_url'].dropna().unique().tolist():
+            logger.debug('Navigating to {}'.format(url))
+            # Connecting to company's url.
+            http_client.fetch(url, self.get_data)
+        self.write("Fetching results for: {}\n".format(url))
+
+
+def setup_logger():
+    """
+    A method to setup logger
+    """
+    # Creating log directory
+    log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'output')
+    log_file = os.path.join(log_dir, 'output-log.log')
+    if not os.path.exists(log_dir):
+        os.mkdir(log_dir)
+
+    # Setting up logger
+    logger = logging.getLogger('MONEYCONTROLAPP')
+    logger.setLevel(logging.DEBUG)
+    logger_fmt = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    # Setting stream handler
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(logger_fmt)
+    logger.addHandler(stream_handler)
+    # Setting file handler
+    file_handler = logging.handlers.RotatingFileHandler(log_file, backupCount=7)
+    file_handler.setFormatter(logger_fmt)
+    logger.addHandler(file_handler)
 
 
 def run_app():
@@ -149,7 +227,13 @@ def run_app():
     ])
 
 if __name__ == "__main__":
+    setup_logger()
+    # Get app port number from command line argument
+    parser = argparse.ArgumentParser(description='Process argument parsers.')
+    parser.add_argument('-p', '--port', default='8888', help='Enter port number.')
+    args = parser.parse_args()
     app = run_app()
-    app.listen(8888)
-    print('Listening at port 8888. Press Ctrl+C to close the server.')
+    app.listen(int(args.port))
+    logger = logging.getLogger('MONEYCONTROLAPP')
+    logger.info('Started running moneycontrol crawler app at port {}. Press Ctrl+C to close the server.'.format(args.port))
     tornado.ioloop.IOLoop.current().start()
